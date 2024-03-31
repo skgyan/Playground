67f39fb29fa9f606956d472616a8301b00ee3026
Sushil Kumar <skgyan.dev@gmail.com>
2024-03-31 16:51:45 -0400

    weakSet

---
+++ WeakSet.js
@@ -0,0 +1,43 @@
+const myWeakSet = new WeakSet();
+
+const name = {'name': 'John'};
+myWeakSet.add(name); 
+
+console.log(myWeakSet.has(name)); // true
+
+
+console.log('------------------ execRecursively ----------------');
+// Execute a callback on everything stored inside an object
+function execRecursively(func, obj, _tracker = new WeakSet()) {
+// Avoid infinite recursion
+    if(_tracker.has(obj)) {
+        return;
+    }
+
+    func(obj);
+
+    if(typeof obj === 'object' && obj) {
+        _tracker.add(obj);
+
+        for (const key in obj) {
+            if (Object.hasOwnProperty.call(obj, key)) {
+                execRecursively(func, obj[key], _tracker);                
+            }
+        }
+
+        _tracker.delete(obj);
+    }
+}
+
+const foo = {
+    foo: "Foo",
+    bar: {
+        bar: "Bar",
+    },
+};
+
+foo.bar.baz = foo; // Circular reference!
+
+execRecursively((obj) => console.log(obj), foo);
+
+  
