5a78c83eedf6f5272ef20c6be022de6317edfb12
Sushil Kumar <skgyan.dev@gmail.com>
2024-04-14 15:56:11 -0400

    update obj shallow deep copy example

--- ShallowDeepCopy.js
+++ ShallowDeepCopy.js
@@ -10,16 +10,15 @@ console.log(originalObj.name === shallowCopy.name); // true (same reference)
 shallowCopy.name.first = 'Bob';
 shallowCopy.age = 31;
 
-console.log(originalObj); // Now 'Bob', since the change is reflected in the original object.
-console.log(shallowCopy);
-
-shallowCopy.name.first = 'Sushil';
-
-console.log(originalObj); // Now 'Bob', since the change is reflected in the original object.
-console.log(shallowCopy);
-
+console.log(originalObj); // Now name is 'Bob', but age is 30, { name: { first: 'Bob', last: 'Kumar' }, age: 30 }
+console.log(shallowCopy); // { name: { first: 'Bob', last: 'Kumar' }, age: 31 }
 
 const deepCopy = JSON.parse(JSON.stringify(originalObj));
 
 console.log(originalObj === deepCopy); // false (different objects)
 console.log(originalObj.name === deepCopy.name); // false (different references)
+
+deepCopy.name.first = 'Sushil';
+
+console.log(originalObj); // { name: { first: 'Bob', last: 'Kumar' }, age: 30 } no change
+console.log(deepCopy); // { name: { first: 'Sushil', last: 'Kumar' }, age: 30 }
